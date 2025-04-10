ef5402b605fddf952d1c1c7b46224421ad7884be
Dipesh8080 <121811003+Dipesh8080@users.noreply.github.com>
2025-04-10 10:25:24 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Import Firebase scripts (via CDN loaded in HTML)
+const firebaseConfig = {
+  apiKey: "AIzaSyAXSrdwDDe8Gg6DCjQ_bPSXu-Sh8whPYsE",
+  authDomain: "scanswaad-28922.firebaseapp.com",
+  projectId: "scanswaad-28922",
+  storageBucket: "scanswaad-28922.firebasestorage.app",
+  messagingSenderId: "677748245747",
+  appId: "1:677748245747:web:3b893e0d07812121aae386"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
